b31afb8ac8a1661446cc37b01c798678e5b72323
Johny <johnyrubens@gmail.com>
2024-04-24 19:28:08 -0300

    instalacao de novos componentes;refatorado sing-int para usar useForm

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,54 @@
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInForm = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export default function Signin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    await new Promise((resolve) => setTimeout(resolve, 200))
+    console.log(data)
+  }
+
   return (
     <>
-      <div>sign-in</div>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-between gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Entre com sua conta
+            </h1>
+          </div>
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-3">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+            <div className="space-y-3">
+              <Label htmlFor="email">Sua senha</Label>
+              <Input id="password" type="password" {...register('password')} />
+            </div>
+            <Button disabled={isSubmitting} type="submit">
+              Acessar Aplicação
+            </Button>
+          </form>
+        </div>
+      </div>
+      <Label>Login</Label>
     </>
   )
 }
